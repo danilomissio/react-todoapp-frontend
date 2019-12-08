03691337871b6650b5b22fb6b944c19fa699dbdb
Danilo Missio <danilo.missio@gmail.com>
2019-12-08 14:29:31 -0300

    Criacao do componente PageHeader

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa história</h2>
+        <p>Nossa história</p>
+        <h2>Missão e Visão</h2>
+        <p>Missão e Visão</p>
+        <h2>Imprensa</h2>
+        <p>Imprensa</p>
     </div>
 )
